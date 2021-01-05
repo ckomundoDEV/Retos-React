36367b11c647bf6272b2efe4123a3c4ac4e0b16c
jose fausto <ckomundo@gmail.com>
2021-01-05 16:43:53 -0700

    colorPicker

---
+++ src/ColorPiker.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import "./ColorPiker.css";
+
+
+function ColorPiker() {
+    const [color, setColor] = useState("#f1f2f5")
+
+
+    const handlClick = e => {
+        setColor(e.target.value)
+        console.log(color);
+    }
+    return (
+        <div className="app" style={{backgroundColor:color}}>
+            <header >
+                <h1 style={{color:color}}>ColorPiker</h1>
+            </header>
+
+            <div className="body">
+                <div className="card">
+                    <p> Crea una página con React.js que muestre 5 cuadros de diferentes colores los cuales al cliquearlos cambien el color de fondo de la página. </p>
+
+                    <div className="colors">
+                        <button onClick={handlClick} style={{backgroundColor:"blue", font:"none"}} value ="blue" className="color"></button>
+                        <button onClick={handlClick} style={{backgroundColor:"red", font:"none"}} value ="red" className="color"></button>
+                        <button onClick={handlClick} style={{backgroundColor:"orange", font:"none"}} value ="orange" className="color"></button>
+                        <button onClick={handlClick} style={{backgroundColor:"purple", font:"none"}} value ="purple" className="color"></button>
+                        <button onClick={handlClick} style={{backgroundColor:"green", font:"none"}} value ="green" className="color"></button>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ColorPiker
